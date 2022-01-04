c2d761658e4792756c8b58349d5773f0f190a1a4
Erik Thiem <erikthiem@gmail.com>
2022-01-03 23:38:53 -0500

    Make the automatic solver pretty much work

--- automatic-solve.py
+++ automatic-solve.py
@@ -1,8 +1,22 @@
 import time
+import json
 from solver import Solver
 puzzle = Solver()
 
 from playwright.sync_api import sync_playwright
+
+def convert_feedback_to_solver_format(feedback):
+    feedback_new_format = ""
+
+    for i in feedback:
+        if (i == 'absent'):
+            feedback_new_format += '-'
+        elif (i == 'present'):
+            feedback_new_format += '!'
+        elif (i == 'correct'):
+            feedback_new_format += 'x'
+
+    return feedback_new_format
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
@@ -13,10 +27,34 @@ with sync_playwright() as p:
     # Dismiss instructions popup
     page.click("game-modal svg")
 
-    # Enter guess
-    guess = 'group'
-    page.keyboard.type(guess)
-    page.keyboard.press('Enter')
+    feedback_solver_format = '-----'
+    remaining_guesses = 6
 
-    # Keep browser open
+    while (feedback_solver_format != 'xxxxxx' or remaining_guesses > 0):
+
+        # Enter guess
+        guess = puzzle.next_guess()
+        page.keyboard.type(guess)
+        page.keyboard.press('Enter')
+
+        # Check guess feedback
+        context.storage_state(path="state.json")
+        browser_state = open("state.json")
+        browser_state_json = json.load(browser_state)
+        all_guess_feedback = json.loads(browser_state_json['origins'][0]['localStorage'][0]['value'])['evaluations']
+        recent_guess_feedback = [i for i in all_guess_feedback if i][-1]
+        feedback_solver_format = convert_feedback_to_solver_format(recent_guess_feedback)
+        print(feedback_solver_format)
+
+        # Find remaining guesses
+        remaining_guesses = len([i for i in all_guess_feedback if not i])
+
+        # Generate the next guess
+        puzzle.get_next_guess(feedback_solver_format)
+
+        # Some time between tries to appreciate the visuals
+        # (also not doing this causes it to progress too fast and crash)
+        time.sleep(5)
+
+    # Keep browser open long enough to see results
     time.sleep(20)
