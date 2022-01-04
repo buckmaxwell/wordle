d7580048a24a8ad86404acb1f55e35813e6f63df
Max Buck <maxbuckdeveloper@gmail.com>
2022-01-03 23:35:11 -0500

    add some tests

--- solve.py
+++ solve.py
@@ -3,9 +3,9 @@ from solver import Solver
 puzzle = Solver()
 
 for _ in range(6):
-    print(f"I will guess '{puzzle.next_guess()}'")
+    print(f"I will guess '{puzzle.guess}'")
     # '----- -- all wrong
     # 'xxxxx' -- all right
     # '!!!!!' -- right letter wrong place
     feedback = input("How did I do?: ")
-    puzzle.get_next_guess(feedback)
+    puzzle.set_next_guess(feedback)
